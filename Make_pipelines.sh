f37900a64c0092e764fa36cadd85c73bc656d3cc
Xiaolong Wang <xw63@h1.carbonate.uits.iu.edu>
2018-06-21 17:43:00 -0400

    Excute me to start

--- Make_pipelines.sh
+++ Make_pipelines.sh
@@ -1,24 +1,23 @@
 echo
 echo
 echo '  ================================================================='
-echo '  #                                                               #'
-echo '  #     To make genome mapping pipelines, execute the             #'
-echo '  # following commands, and follow the instructions they give:    #'
-echo '  #                                                               #'
-echo '  #     To use bwa:                                               #'
-echo '  #                                                               #'
-echo '                 perl MGMP.pl bwa path sampleID   '
-echo '  #                                                               #'
-echo '  #     To use novalign:                                          #'
-echo '  #                                                               #'
+echo '                                                                 '
+echo '       To make genome mapping pipelines, execute the             '
+echo '   following commands, and follow the instructions they give:    '
+echo '                                                                 '
+echo '       To use bwa:               								'
+echo '                                                                 '
+echo '         		perl MGMP.pl bwa path sampleID   '
+echo '                                                                 '
+echo '       To use novalign:          								'
+echo '                                                                 '
 echo '      		perl MGMP.pl novalign path sampleID '
-echo '  #                                                               #'
-echo '  #     To use hisat2:                                            #'
-echo '  #                                                               #'
+echo '                                                                 '
+echo '       To use hisat2:            								'
+echo '                                                                 '
 echo '       		perl MGMP.pl hisat path sampleID   '
-echo '  #                                                               #'
+echo '                                                                 '
 echo '  ================================================================='
-echo  Please copy and execute one of the above commands with appropriate args.
+echo 			Please copy and execute one of the above commands.
 echo 
-
 
